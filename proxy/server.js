cc98f6d54260a8ca24aefc1cc12b07e5f8938a30
Elizabeth Li <sincerelylibby@gmail.com>
2018-06-16 12:54:25 -0700

    Refactor code to route css through server.

--- proxy/server.js
+++ proxy/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
@@ -8,55 +7,54 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/:id', express.static(path.join(__dirname, '/public')));
+app.use('/:id', express.static(path.resolve(__dirname, './public')));
 
 app.get('/:id/buying', (req, res) => {
-  axios.get(`http://localhost:8000/${req.params.id}/dist/buying.js`)
+  axios.get(`http://fecpetsybuying-env.ba2g3p3aep.us-west-1.elasticbeanstalk.com/${req.params.id}/dist/buying.js`)
     .then(response => res.status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.get('/:id/buyingcss', (req, res) => {
-  axios.get(`http://localhost:8000/${req.params.id}/dist/styles.css`)
-    .then(response => res.status(200).send(response.data))
+  axios.get(`http://fecpetsybuying-env.ba2g3p3aep.us-west-1.elasticbeanstalk.com/${req.params.id}/dist/styles.css`)
+    .then(response => res.set('Content-Type', 'text/css').status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.get('/:id/details', (req, res) => {
-  axios.get(`http://localhost:8000/${req.params.id}/details`)
+  axios.get(`http://fecpetsybuying-env.ba2g3p3aep.us-west-1.elasticbeanstalk.com/${req.params.id}/details`)
     .then(response => res.status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.get('/:id/jesseReviews', (req, res) => {
-  axios.get(`http://localhost:3001/${req.params.id}/dist/main.js`)
+  axios.get(`http://fecpetsyreviews-env-1.z4rdkvqtp8.us-west-1.elasticbeanstalk.com/${req.params.id}/dist/main.js`)
     .then(response => res.status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.get('/:id/reviewscss', (req, res) => {
-  axios.get(`http://localhost:3001/${req.params.id}/dist/styles.css`)
-    .then(response => res.status(200).send(response.data))
+  axios.get(`http://fecpetsyreviews-env-1.z4rdkvqtp8.us-west-1.elasticbeanstalk.com/${req.params.id}/dist/styles.css`)
+    .then(response => res.set('Content-Type', 'text/css').status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.get('/:id/reviews', (req, res) => {
-  axios.get(`http://localhost:3001/${req.params.id}/reviews`)
+  axios.get(`http://fecpetsyreviews-env-1.z4rdkvqtp8.us-west-1.elasticbeanstalk.com/${req.params.id}/reviews`)
     .then(response => res.status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.post('/:id/reviews', (req, res) => {
-  axios.post(`http://localhost:3001/${req.params.id}/reviews`, req.body)
+  axios.post(`http://fecpetsyreviews-env-1.z4rdkvqtp8.us-west-1.elasticbeanstalk.com/${req.params.id}/reviews`, req.body)
     .then(response => res.status(201).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.put('/:id/reviews', (req, res) => {
-  axios.put(`http://localhost:3001/${req.params.id}/reviews`, req.body)
+  axios.put(`http://fecpetsyreviews-env-1.z4rdkvqtp8.us-west-1.elasticbeanstalk.com/${req.params.id}/reviews`, req.body)
     .then(response => res.status(204).end())
     .catch(error => res.status(500).send(error));
 });
@@ -69,13 +67,13 @@ app.get('/:id/xueProducts', (req, res) => {
 
 app.get('/:id/shopcss', (req, res) => {
   axios.get(`http://localhost:5000/${req.params.id}/style.css`)
-    .then(response => res.status(200).send(response.data))
+    .then(response => res.set('Content-Type', 'text/css').status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
 app.get('/:id/shopproducts', (req, res) => {
   axios.get(`http://localhost:5000/${req.params.id}/shopproducts`)
-    .then(response => res.status(200).send(response.data))
+    .then(response => res.set('Content-Type', 'text/css').status(200).send(response.data))
     .catch(error => res.status(500).send(error));
 });
 
